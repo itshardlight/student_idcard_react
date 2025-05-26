d3361f0e9a1f0b7f941840a622cf74fe39608e1b
itshardlight <unishmhrjan@gmail.com>
2025-05-26 08:21:21 +0545

    left-sidebar completed

--- src/mysrc/Dashboard.js
+++ src/mysrc/Dashboard.js
@@ -1,32 +1,38 @@
-import React from 'react';
-
+import React from "react";
+import './Dashboard.css';
 function Dashboard() {
   return (
     <>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-    </div>
+    <div className="webpage">
+  <div className="heading row d-flex align-items-center p-3 bg-primary text-white">
+    <div className="col-6 text-start">STUDENT CARD HUB</div>
+    <div className="col-6 text-end">superadmin</div>
   </div>
-</nav>
+
+  <div className="body bg-danger row" style={{ minHeight: '600px' }}>
+    <div className="col-2 bg-light p-3">
+      <div className="mb-2 ms-3">Dashboard</div>
+      <div className="mb-2 ms-3">All Student Cards</div>
+      <hr />
+      <div className="mb-2 ms-3">School Management</div>
+      <div className="mb-2 ms-3">Manage Schools</div>
+      <div className="mb-2 ms-3">Add New School</div>
+       <hr />
+      <div className="mb-2 ms-3">User Management</div>
+      <div className="mb-2 ms-3">Settings</div>
+      <div className="mb-2 ms-3">Log Out</div>
+    </div>
+    
+<div className="right-content col-10 p-3">
+  <div className="row bg-light align-items-center p-2 ps-4">
+    <div className="col ps-3">Dashboard</div>
+  </div>
+</div>
+
+
+  </div>
+</div>
+
     </>
   );
 }
