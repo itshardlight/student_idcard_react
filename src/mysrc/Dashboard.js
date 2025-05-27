943d696ed45b342f31766030534bc0a3291d98f1
itshardlight <unishmhrjan@gmail.com>
2025-05-27 11:22:48 +0545

    fixed the padding in mv

--- src/mysrc/Dashboard.js
+++ src/mysrc/Dashboard.js
@@ -4,8 +4,6 @@ function Dashboard() {
   return (
     <>
       <div className="webpage">
-   
-
         <div className="body row" style={{ minHeight: "600px" }}>
           <nav className="navbar navbar-light bg-light d-md-none">
             <div className="container-fluid">
@@ -23,10 +21,14 @@ function Dashboard() {
             </div>
           </nav>
 
-          <div className="collapse left-content d-md-block col-md-2 bg-white p-3 "
+          <div
+            className="collapse left-content d-md-block col-md-2 bg-white p-3 "
             id="sidebarMenu"
-          > <div className="heading row d-flex align-items-center p-3 bg-primary text-white">
-                 <div className="text-start">STUDENT CARD HUB</div></div>
+          >
+            {" "}
+            <div className="heading row d-flex align-items-center p-3 bg-primary text-white">
+              <div className="text-start">STUDENT CARD HUB</div>
+            </div>
             <div className="mb-2 ms-3 p-2 mt-5">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -135,19 +137,23 @@ function Dashboard() {
             </div>
           </div>
 
-          <div className="right-content col-12 col-md-10 p-1 p-md-3">
-                 <div className="heading row d-flex align-items-center p-3 bg-primary text-white">
-     
-          <div className="col text-end">superadmin</div>
-        </div>
-                 <div className="row"> <div className="col-3 text-center p-3">    Dashboard</div></div>
+          <div className="right-content col-12 col-md-10 p-3">
+            <div className="heading row d-flex align-items-center p-3 bg-primary text-white">
+              <div className="col text-end">superadmin</div>
+            </div>
+            <div className="row">
+              {" "}
+              <div className="col-3 text-center p-3"> Dashboard</div>
+            </div>
 
             <div className="row bg-light align-items-center p-2 ps-4">
-         
-              <div className="container py-3 col-10">
+              <div className="container py-3 col-md-10">
                 <div className="container bg-white rounded p-4 mb-4">
                   <div className="col mb-2">Welcome To The Admin Panel</div>
-                 <div className="col"> <button class="btn btn-primary">View Student Cards</button></div>
+                  <div className="col">
+                    {" "}
+                    <button class="btn btn-primary">View Student Cards</button>
+                  </div>
                 </div>
                 <div className="container bg-white rounded p-4 mb-4 ">
                   <div className="col mb-2"> Quick Stats</div>
@@ -156,9 +162,20 @@ function Dashboard() {
                       <div className="border border-primary p-3 h-100 d-flex flex-column ">
                         <div className="row">
                           <div className="col-2">
-                           <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-blue-500 icon2"  fill="none" viewBox="0 0 24 24" stroke="white">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V8a2 2 0 00-2-2h-5m-4 0V5a2 2 0 114 0v1m-4 0a2 2 0 104 0m-5 8a2 2 0 100-4 2 2 0 000 4z"></path>
-                                    </svg>
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              class="h-6 w-6 text-blue-500 icon2"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              stroke="white"
+                            >
+                              <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M10 6H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V8a2 2 0 00-2-2h-5m-4 0V5a2 2 0 114 0v1m-4 0a2 2 0 104 0m-5 8a2 2 0 100-4 2 2 0 000 4z"
+                              ></path>
+                            </svg>
                           </div>
                           <div className="col-10">
                             {" "}
@@ -170,13 +187,24 @@ function Dashboard() {
                         </div>
                       </div>
                     </div>
-                     <div className="col-12 col-md-4">
+                    <div className="col-12 col-md-4">
                       <div className="border border-primary p-3 h-100 d-flex flex-column ">
                         <div className="row">
                           <div className="col-2">
-                          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-green-500 icon2" fill="none" viewBox="0 0 24 24" stroke="white">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-                                    </svg>
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              class="h-6 w-6 text-green-500 icon2"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              stroke="white"
+                            >
+                              <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
+                              ></path>
+                            </svg>
                           </div>
                           <div className="col-10">
                             {" "}
@@ -188,13 +216,24 @@ function Dashboard() {
                         </div>
                       </div>
                     </div>
-                      <div className="col-12 col-md-4">
+                    <div className="col-12 col-md-4">
                       <div className="border border-primary p-3 h-100 d-flex flex-column ">
                         <div className="row">
                           <div className="col-2">
-                          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-purple-500 icon2" fill="none" viewBox="0 0 24 24" stroke="white">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2"></path>
-                                    </svg>
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              class="h-6 w-6 text-purple-500 icon2"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              stroke="white"
+                            >
+                              <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2"
+                              ></path>
+                            </svg>
                           </div>
                           <div className="col-10">
                             {" "}
@@ -203,7 +242,7 @@ function Dashboard() {
                         </div>
                         <div className="col">
                           <div>0</div>
-                            <div>New submissions this month</div>
+                          <div>New submissions this month</div>
                         </div>
                       </div>
                     </div>
@@ -320,13 +359,24 @@ function Dashboard() {
                         </div>
                       </div>
                     </div>
-                     <div className="col-12 col-md-4">
+                    <div className="col-12 col-md-4">
                       <div className="border border-primary p-3 h-100 d-flex flex-column ">
                         <div className="row">
                           <div className="col-2">
-                           <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-amber-500 icon2" fill="none" viewBox="0 0 24 24" stroke="white">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"></path>
-                                        </svg>
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              class="h-6 w-6 text-amber-500 icon2"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              stroke="white"
+                            >
+                              <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"
+                              ></path>
+                            </svg>
                           </div>
                           <div className="col-10">
                             {" "}
@@ -338,13 +388,24 @@ function Dashboard() {
                         </div>
                       </div>
                     </div>
-                      <div className="col-12 col-md-4">
+                    <div className="col-12 col-md-4">
                       <div className="border border-primary p-3 h-100 d-flex flex-column ">
                         <div className="row">
                           <div className="col-2">
-                           <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-pink-500 icon2" fill="none" viewBox="0 0 24 24" stroke="white">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-                                        </svg>
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              class="h-6 w-6 text-pink-500 icon2"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              stroke="white"
+                            >
+                              <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                              ></path>
+                            </svg>
                           </div>
                           <div className="col-10">
                             {" "}
@@ -353,7 +414,7 @@ function Dashboard() {
                         </div>
                         <div className="row">
                           <div>test123</div>
-                            <div>2 days ago</div>
+                          <div>2 days ago</div>
                         </div>
                       </div>
                     </div>
